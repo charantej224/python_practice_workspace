e6350277676d9533b2109610ecd075a40843b19e
charantej224 <charantej.career@gmail.com>
2020-06-03 22:54:04 -0500

    tagtog experiments

---
+++ tag_tog_experiments/ann_import_tag_tog.py
@@ -0,0 +1,13 @@
+import requests
+
+tagtogAPIUrl = "https://www.tagtog.net/-api/documents/v1"
+auth = requests.auth.HTTPBasicAuth(username='charantej', password='Password*01')
+params = {'project': 'sample', 'owner': 'charantej', 'output': 'null', 'format': 'default-plus-annjson'}
+with open('ann.json', 'r') as f:
+    annotations = f.read()
+files = {
+    'ann': ('text.ann.json', annotations),
+    'plain': ('text.txt', open('./text.txt'))
+}
+
+response = requests.post(tagtogAPIUrl, params=params, auth=auth, files=files)
