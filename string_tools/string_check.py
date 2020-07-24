938d021c0cadc973324f7988c8a415d6d39eb162
charantej224 <charantej.career@gmail.com>
2020-07-23 22:09:14 -0500

    random results

--- string_tools/string_check.py
+++ string_tools/string_check.py
@@ -1,5 +1,16 @@
-input_str = "Hi,I'm charan"
-check_str = "charan"
-index = input_str.find(check_str)
-print(input_str[index:index + len(check_str)])
-print(input_str.split())
+# input_str = "Hi,I'm charan"
+# check_str = "charan"
+# index = input_str.find(check_str)
+# print(input_str[index:index + len(check_str)])
+# print(input_str.split())
+
+map_value = {"name": "charan",
+             "password": "value"}
+
+if "name" in map_value:
+    with open("file.txt", "a+") as f:
+        val = f.read()
+        val = val + "\n" + map_value["name"]
+        f.write(val)
+        f.close()
+    print(map_value["name"])
